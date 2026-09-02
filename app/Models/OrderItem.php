428c1129d0bc7e7eaaaa5a7335bf8d773a3227ab
Jason <13209003+j4ysanchez@users.noreply.github.com>
2026-09-01 20:29:54 -0600

    Create OrderItem model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = ['order_id', 'pizza_id', 'quantity', 'unit_price'];
+
+    public function order(): BelongsTo {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function pizza(): BelongsTo {
+        return $this->belongsTo(Pizza::class);
+    }
+
 }
