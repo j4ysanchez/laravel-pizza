08f33c6d9c592286c56a0133da9b80c54d34354a
Jason <13209003+j4ysanchez@users.noreply.github.com>
2026-09-01 20:22:02 -0600

    Create pizza model

--- app/Models/Pizza.php
+++ app/Models/Pizza.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Pizza extends Model
 {
     /** @use HasFactory<\Database\Factories\PizzaFactory> */
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'image'];
+
+    public function toppings(): BelongsToMany {
+        return $this->belongsToMany(Topping::class);
+    }
+
+    public function orderItems(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
 }
