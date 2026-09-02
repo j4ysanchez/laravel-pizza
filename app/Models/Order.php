7d3252f94eb7db90cdac2e20290a80d2fcadb539
Jason <13209003+j4ysanchez@users.noreply.github.com>
2026-09-01 20:25:28 -0600

    Created order and Topping models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
     /** @use HasFactory<\Database\Factories\OrderFactory> */
     use HasFactory;
+
+    protected $fillable = ['customer_name', 'customer_email', 'status', 'total'];
+
+    public function items(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- app/Models/Topping.php
+++ app/Models/Topping.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Topping extends Model
 {
     /** @use HasFactory<\Database\Factories\ToppingFactory> */
     use HasFactory;
+
+    protected $fillable = ['name', 'price'];
+
+    public function pizzas(): BelongsToMany {
+        return $this->belongsToMany(Pizza::class);
+    }
 }
